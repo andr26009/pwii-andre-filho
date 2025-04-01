82e27d09cab86bda7dacc1817a9e9f5091051c5c
André Schwartz Filho <andrecacador2@gmail.com>
2025-04-01 16:30:39 -0300

    Criamos arquivo para ensinar datatypes

---
+++ aula 01/concatenar.php
@@ -0,0 +1,9 @@
+<?php
+  ECHO "Hello World!<br>";
+  echo "Hello World!<br>";
+  EcHo "Hello World!<br>";
+  $nome= "André";
+  $sobrenome= "Schwartz";
+  $numero= 4;
+  echo "${nome} <h1> ${sobrenome} </h1>";
+  echo $numero . "<p> " . $nome . 
---
+++ aula 02/datatypes.php
@@ -0,0 +1,24 @@
+<?php
+  $x = "Hello world!"
+  $y = 'Hello World!'
+
+  var_dump($x)
+  echo "<br>";
+  var_dump($y);
+
+  $x = 1;
+
+  var_dump($x);
+   echo "<br><br>";
+
+   $y = ['André', 'Matheus', 4]
+
+   var_dump($y)
+   echo "<br>";
+   echo $y[1]
+
+   $y = {
+      'nome' : "Baldo",
+      "idade" : 16,
+   }
+ ?>
